9cc45425634c04d9e0e1c9587e57d09fcac48b6e
sabbir1791 <sabbirh8181@gmail.com>
2023-11-08 06:22:12 +0600

    client side update click to curd oparetion update

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-
 
 // middleware
 
@@ -30,73 +29,92 @@ async function run() {
     const bookCollection = client.db("libraryPulse").collection("book");
     const borrowCollection = client.db("libraryPulse").collection("borrow");
 
-
-
-    app.get('/book', async(req, res) =>{
-        const cursor = bookCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get("/book", async (req, res) => {
+      const cursor = bookCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     });
 
-    app.get('/book/:id', async(req, res)=>{
+    app.get("/book/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await bookCollection.findOne(query);
       res.send(result);
-    })
-
-    app.post('/book', async(req, res) =>{
-        const newBooks = req.body;
-        const result = await bookCollection.insertOne(newBooks);
-        res.send(result);
     });
 
+    app.put("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const bookUpdate = req.body;
+      console.log(bookUpdate);
+      
+      try {
+        const result = await bookCollection.updateOne(filter, { $set: bookUpdate });
+    
+        if (result.matchedCount > 0 && result.modifiedCount > 0) {
+          res.status(200).json({ success: true, message: "Book updated successfully" });
+        } else {
+          res.status(404).json({ success: false, message: "Book not found or not modified" });
+        }
+      } catch (error) {
+        console.error("Error updating book:", error);
+        res.status(500).json({ success: false, message: "Internal server error" });
+      }
+    });
+    
+    
+    
+
+    app.post("/book", async (req, res) => {
+      const newBooks = req.body;
+      const result = await bookCollection.insertOne(newBooks);
+      res.send(result);
+    });
 
     // Borrow api
-    app.get('/borrow', async(req, res) =>{
+    app.get("/borrow", async (req, res) => {
       const cursor = borrowCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-  });
+    });
 
-  app.post('/borrow', async(req, res) =>{
-    const {newBorrow, book} = req.body;
-    const result = await borrowCollection.insertOne({newBorrow, book});
-    res.send(result);
-});
+    app.post("/borrow", async (req, res) => {
+      const { newBorrow, book } = req.body;
+      const result = await borrowCollection.insertOne({ newBorrow, book });
+      res.send(result);
+    });
 
-app.patch('/book/:id', async (req, res) => {
+  
+app.put("/book/:id", async (req, res) => {
   const id = req.params.id;
   const filter = { _id: new ObjectId(id) };
-  const { quantity: updatedQuantity } = req.body;
-  
-  const update = {
-    $set: { quantity: updatedQuantity }
-  };
+  const { quantity } = req.body; 
 
   try {
+    const update = {
+      $set: { quantity: quantity }, 
+    };
+
     const result = await bookCollection.updateOne(filter, update);
 
     if (result.modifiedCount === 1) {
-      res.json({ quantity: updatedQuantity });
+      res.status(200).json({ message: "Quantity updated successfully" });
     } else {
-      res.status(404).json({ message: 'Book not found' });
+      res.status(404).json({ message: "Book not found" });
     }
   } catch (error) {
-    console.error('Error updating quantity:', error);
-    res.status(500).json({ message: 'Server error' });
+    console.error("Error updating quantity:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
 
-
-app.delete('/borrow/:id', async (req, res) => {
-  const id = req.params.id;
-  const query = { _id: new ObjectId(id) }
-  const result = await borrowCollection.deleteOne(query);
-  res.send(result);
-});
-
+    app.delete("/borrow/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await borrowCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
